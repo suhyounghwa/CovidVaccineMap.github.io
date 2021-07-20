7c981da15d5928cf9deba80d821c0fa6095b939f
YoonsunC <87630630+YoonsunC@users.noreply.github.com>
2021-07-20 14:55:18 +0900

    Update DataCrawling.py

--- DataCrawling.py
+++ DataCrawling.py
@@ -2,7 +2,9 @@ import urllib.request
 import datetime
 import json
 
-def Get_Request_Url(url):# 데이터 크롤링
+
+# 코로나 백신접종 센터 데이터 크롤링(공공기관/위탁의료기관)
+def Get_Request_Url(url):
     req = urllib.request.Request(url) #검색 URL 경로 지정
 
     try:
@@ -15,16 +17,20 @@ def Get_Request_Url(url):# 데이터 크롤링
         print("[%s] 오류 : %s " % datetime.datetime.now(), url)
         return None
 
-def GetGoVSearchResult(endPoint, pageValue, perPageValue, keyValue) : # 사전 준비 작업 및 분류
+
+# 사전 준비 작업 및 분류(크롤링을 위한 url 생성)
+def GetGoVSearchResult(endPoint, pageValue, perPageValue, keyValue) :
     paraData = "page=" + str(pageValue)
     paraData += "&perPage=" + str(perPageValue)
-    
+    # paraData에 문자형식으로 pageValue, perPageValue 저장
 
-    url = endPoint + paraData + keyValue
+    url = endPoint + paraData + keyValue # url 생성
 
     resultData = Get_Request_Url(url)
+    # resultData = Get_Request_Url 함수에서 크롤링해온 데이터
 
     if(resultData == None):
         return None
     else:
         return json.loads(resultData)
+    # resultData파일 json파일 형식으로 리턴
