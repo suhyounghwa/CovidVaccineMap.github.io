e114547356a505b345c62634e5aac5d9ba0218f1
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2021-07-22 11:33:24 +0900

    naverMapData 함수 이동

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,29 +1,24 @@
-from ssl import create_default_context
 import pandas as pd
 
-def GetCenterData(resultData, address, name, number,lat,lng,city):
+def GetCenterData(resultData, addr, name, num, lat, lng, city):
     # json 파일의 data 키값 데이터를 DataFrame 형태로 저장
     tempResult = pd.DataFrame(resultData['data'])
-    # 키 값으로 주소/센터명/번호 값 추출하여 DataFrame 형태로 저장
-    if(lat!=None):
-        tempResult = tempResult[[str(address), str(name), str(number),str(lat),str(lng)]]
     # 컬럼 확인 후 입력한 지역명의 데이터만 저장
-        centerResult = tempResult[tempResult['address'].str.contains(city)]
-        centerAddr = centerResult['address'].values
-        centerName = centerResult['centerName'].values
-        centerNum = centerResult['phoneNumber'].values
-        centerlng = centerResult['lng'].values
-        centerlat = centerResult['lat'].values
-        
-    
+    centerResult = tempResult[tempResult[str(addr)].str.contains(city)]
+    # 주소/센터명/번호/위도/경도 값만 추출하여 DataFrame 형태로 저장
+    centerAddr = centerResult[str(addr)].values
+    centerName = centerResult[str(name)].values
+    centerNum = centerResult[str(num)].values
+    if(lat!=None):
+        centerlng = centerResult[lng].values
+        centerlat = centerResult[lat].values    
     else:
-        centerResult = tempResult[tempResult['orgZipaddr'].str.contains(city)]
-        centerAddr = centerResult['orgZipaddr'].values
-        centerName = centerResult['orgnm'].values
-        centerNum = centerResult['orgTlno'].values
         centerlat= None
         centerlng=None
     return [centerAddr, centerName, centerNum,centerlat,centerlng]
 
-
-#def GetEachData(resultData, address, name, number, city):
+def GetLngLatData(LocationData):
+        # 정제 - JSON 파일 분석 후 데이터 찾기 - 위도 / 경도 값
+        xdata = LocationData['addresses'][0]['x']
+        ydata = LocationData['addresses'][0]['y']
+        return [ydata, xdata]
--- DataCrawling.py
+++ DataCrawling.py
@@ -2,11 +2,16 @@ import urllib.request
 import datetime
 import json
 
+client_id = "5l5yadvenl"
+client_secret = "ygf36whrwrx4XQGFoGaepijGIrvEdHWIvgCgnmF8"
+baseUrl = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode"
 
 # 코로나 백신접종 센터 데이터 크롤링(공공기관/위탁의료기관)
 def Get_Request_Url(url):
     req = urllib.request.Request(url) #검색 URL 경로 지정
-
+    req.add_header("X-NCP-APIGW-API-KEY-ID", client_id) # 경로 접근하기 위한 아이디 - naver에서 발급
+    req.add_header("X-NCP-APIGW-API-KEY", client_secret) #경로 접근하기 위한 비밀번호 - naver에서 발급
+    
     try:
         response = urllib.request.urlopen(req) # URL을 통해 데이터 요청해서 결과 받음
         if response.getcode() == 200: # 성공코드 : 200
@@ -34,3 +39,13 @@ def GetGoVSearchResult(endPoint, pageValue, perPageValue, keyValue) :
     else:
         return json.loads(resultData)
     # resultData파일 json파일 형식으로 리턴
+
+# 위도/경도 값 출력을 위한 사전 준비 작업
+def GetGeoLocationData(addr):
+        paraData = "?query=%s" % urllib.parse.quote(addr)
+        resulturl = baseUrl + paraData
+        resultData = Get_Request_Url(resulturl)
+        if(resultData == None):
+            return None
+        else: 
+            return json.loads(resultData)
