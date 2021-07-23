7277f7275e30cdad950421027890195034fccac5
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2021-07-23 14:01:06 +0900

    메인 함수 수정

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from DataCrawling import *
 from DataCleaning import *
 import pandas as pd
 import folium
-<<<<<<< HEAD
-import math
-=======
->>>>>>> bc905bf806fd812b7fac1a88deb6bc25866ab07b
 import time
 
 def Main():
@@ -29,61 +25,8 @@ def Main():
     # 센터주소, 센터명, 센터전화번호 데이터를 이용하여 데이터프레임 생성 
     jsonCleaningData = pd.DataFrame(data=list(zip(addr, name, num, lat, lng)), columns = ['Addr', 'name', 'num','lat','lng'])    
     jsonCleaningData2 = pd.DataFrame(data=list(zip(addr2, name2, num2, lat2, lng2)), columns = ['Addr', 'name', 'num','lat','lng'])     
-
-<<<<<<< HEAD
-    jsonCleaningData3= jsonCleaningData.append(jsonCleaningData2,ignore_index=True)
-    print("time:",time.time()-start)
-    jsonCleaningData3.iloc[2245] = 0
-    #jsonCleaningData4 = jsonCleaningData3.drop(jsonCleaningData3.index[2245])
-    print(jsonCleaningData3.head(2246))
-    print("정제끝")
-    # i=0
-    # inputCenterName=input("센터이름 입력 :")
-    # for x in jsonCleaningData3['name']:   
-    #   if (x==str(inputCenterName)):        
-    #     if  math.isnan(float(jsonCleaningData3['lat'][i])):
-    #       Location=GetLngLatData(GetGeoLocationData(jsonCleaningData3['Addr'][i]))
-    #       map_data=folium.Map(Location,tzoom_start=12)
-    #       marker= folium.Marker(Location, popup=jsonCleaningData3['num'][i], tooltip=jsonCleaningData3['name'][i],icon=folium.Icon(color="red"))
-    #     else:
-    #       Location=[jsonCleaningData3['lat'][i],jsonCleaningData3['lng'][i]]
-    #       map_data=folium.Map(Location,zoom_start=12)
-    #       marker= folium.Marker(Location,color='blue' ,popup=jsonCleaningData3['num'][i], tooltip=jsonCleaningData3['name'][i],icon=folium.Icon(color="green")) 
-      
-    #     marker.add_to(map_data)
-    #     map_data.save(r'c:\module1\navermap4.html')
-    #     print("[%s] 저장 성공 : " % datetime.datetime.now())
-    #     break
-    #   i=i+1
-    
-
-    i=0
-    map_data = folium.Map([37.56595045963169, 126.98918361888224],zoom_start=12)
-    count = 0
-    for x in jsonCleaningData3['Addr']:        
-        if math.isnan(float(jsonCleaningData3['lat'][i])):
-          Location=GetLngLatData(GetGeoLocationData(jsonCleaningData3['Addr'][i]))
-          marker= folium.Marker(Location, popup=jsonCleaningData3['num'][i], tooltip=jsonCleaningData3['name'][i],icon=folium.Icon(color="red"))
-          marker.add_to(map_data)
-        else:
-          Location=[jsonCleaningData3['lat'][i],jsonCleaningData3['lng'][i]]
-          marker= folium.Marker(Location,color='blue' ,popup=jsonCleaningData3['num'][i], tooltip=jsonCleaningData3['name'][i],icon=folium.Icon(color="green")) 
-          marker.add_to(map_data)
-        i=i+1
-        count += 1
-        print(count)
-=======
-    #jsonCleaningData3= jsonCleaningData.append(jsonCleaningData2,ignore_index=True)
-    #print(jsonCleaningData3.iloc[2245])
-    #print("time:",time.time()-start)
-    #jsonCleaningData3.iloc[2245] = 0
-
-    #jsonCleaningData4 = jsonCleaningData3.drop(jsonCleaningData3.index[2245])
-    #print(jsonCleaningData3.head(2246))
-    #print("정제끝")
     
     map_data = folium.Map([37.56595045963169, 126.98918361888224],zoom_start=12)
-    #count = 0
 
     for i in range(0, len(jsonCleaningData)):
       Location=[jsonCleaningData['lat'][i],jsonCleaningData['lng'][i]]
@@ -94,10 +37,7 @@ def Main():
       Location=[jsonCleaningData2['lat'][i],jsonCleaningData2['lng'][i]]
       marker= folium.Marker(Location, popup=jsonCleaningData2['num'][i], tooltip=jsonCleaningData2['name'][i],icon=folium.Icon(color="red"))
       marker.add_to(map_data)
-      #count += 1
-      #print(count)
 
->>>>>>> bc905bf806fd812b7fac1a88deb6bc25866ab07b
     map_data.save(r'c:\module1\navermap4.html')
   
     print("[%s] 저장 성공 : " % datetime.datetime.now())
@@ -106,8 +46,4 @@ def Main():
 if __name__ == '__main__':
     Main()
 
-<<<<<<< HEAD
  
-=======
- 
->>>>>>> bc905bf806fd812b7fac1a88deb6bc25866ab07b
