bc905bf806fd812b7fac1a88deb6bc25866ab07b
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2021-07-23 12:32:23 +0900

    GetGeoLocationData 데이터 정제 파일로 이동

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,5 +1,19 @@
 import pandas as pd
+from DataCrawling import GetGeoLocationData
+import numpy as np
 
+'''
+    백신접종센터 데이터 정제 함수 (주소/센터명/전화번호/위도/경도 추출)
+    resultData - 백신접종센터 JSON 데이터
+    addr - JSON 파일의 주소 변수 명
+    name - JSON 파일의 센터명 변수 명
+    num - JSON 파일의 전화번호 변수 명
+    lat - JSON 파일의 위도 변수 명
+    lng - JSON 파일의 경도 변수 명
+    city - 정제할 지역명 
+    Main에서의 함수 호출 예 
+         GetCenterData(jsonSearchResult, "address","centerName","phoneNumber","lat","lng","서울특별시")
+'''
 def GetCenterData(resultData, addr, name, num, lat, lng, city):
     # json 파일의 data 키값 데이터를 DataFrame 형태로 저장
     tempResult = pd.DataFrame(resultData['data'])
@@ -13,10 +27,22 @@ def GetCenterData(resultData, addr, name, num, lat, lng, city):
         centerlng = centerResult[lng].values
         centerlat = centerResult[lat].values    
     else:
-        centerlat= None
-        centerlng=None
+        centerlng = []
+        centerlat = []
+        for addrStr in centerAddr :
+            centerLocation = GetLngLatData(GetGeoLocationData(addrStr))
+            centerlat.append(centerLocation[0])
+            centerlng.append(centerLocation[1])
+        #centerlat= None
+        #centerlng=None
     return [centerAddr, centerName, centerNum,centerlat,centerlng]
 
+'''
+    민간병원 데이터 정제 함수 (위도/경도 추출)
+    LocationData - 위치 JSON 데이터 
+    Main에서의 함수 호출 예
+         DataCleaning.GetLngLatData(LocationData)
+'''
 def GetLngLatData(LocationData):
         # 정제 - JSON 파일 분석 후 데이터 찾기 - 위도 / 경도 값
         xdata = LocationData['addresses'][0]['x']
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-#from naverDataMap import *
-from numpy import NaN
 from DataCrawling import *
 from DataCleaning import *
 import pandas as pd
 import folium
-import math
 import time
 
 def Main():
@@ -27,49 +24,32 @@ def Main():
     
     # 센터주소, 센터명, 센터전화번호 데이터를 이용하여 데이터프레임 생성 
     jsonCleaningData = pd.DataFrame(data=list(zip(addr, name, num, lat, lng)), columns = ['Addr', 'name', 'num','lat','lng'])    
-    jsonCleaningData2 = pd.DataFrame(data=list(zip(addr2, name2, num2)), columns = ['Addr', 'name', 'num']) 
+    jsonCleaningData2 = pd.DataFrame(data=list(zip(addr2, name2, num2, lat2, lng2)), columns = ['Addr', 'name', 'num','lat','lng'])     
 
-    jsonCleaningData3= jsonCleaningData.append(jsonCleaningData2,ignore_index=True)
-    print("time:",time.time()-start)
-    jsonCleaningData3.iloc[2245] = 0
+    #jsonCleaningData3= jsonCleaningData.append(jsonCleaningData2,ignore_index=True)
+    #print(jsonCleaningData3.iloc[2245])
+    #print("time:",time.time()-start)
+    #jsonCleaningData3.iloc[2245] = 0
+
     #jsonCleaningData4 = jsonCleaningData3.drop(jsonCleaningData3.index[2245])
-    print(jsonCleaningData3.head(2246))
-    print("정제끝")
-    # i=0
-    # inputCenterName=input("센터이름 입력 :")
-    # for x in jsonCleaningData3['name']:   
-    #   if (x==str(inputCenterName)):        
-    #     if  math.isnan(float(jsonCleaningData3['lat'][i])):
-    #       Location=GetLngLatData(GetGeoLocationData(jsonCleaningData3['Addr'][i]))
-    #       map_data=folium.Map(Location,tzoom_start=12)
-    #       marker= folium.Marker(Location, popup=jsonCleaningData3['num'][i], tooltip=jsonCleaningData3['name'][i],icon=folium.Icon(color="red"))
-    #     else:
-    #       Location=[jsonCleaningData3['lat'][i],jsonCleaningData3['lng'][i]]
-    #       map_data=folium.Map(Location,zoom_start=12)
-    #       marker= folium.Marker(Location,color='blue' ,popup=jsonCleaningData3['num'][i], tooltip=jsonCleaningData3['name'][i],icon=folium.Icon(color="green")) 
-      
-    #     marker.add_to(map_data)
-    #     map_data.save(r'c:\module1\navermap4.html')
-    #     print("[%s] 저장 성공 : " % datetime.datetime.now())
-    #     break
-    #   i=i+1
+    #print(jsonCleaningData3.head(2246))
+    #print("정제끝")
     
-
-    i=0
     map_data = folium.Map([37.56595045963169, 126.98918361888224],zoom_start=12)
-    count = 0
-    for x in jsonCleaningData3['Addr']:        
-        if math.isnan(float(jsonCleaningData3['lat'][i])):
-          Location=GetLngLatData(GetGeoLocationData(jsonCleaningData3['Addr'][i]))
-          marker= folium.Marker(Location, popup=jsonCleaningData3['num'][i], tooltip=jsonCleaningData3['name'][i],icon=folium.Icon(color="red"))
-          marker.add_to(map_data)
-        else:
-          Location=[jsonCleaningData3['lat'][i],jsonCleaningData3['lng'][i]]
-          marker= folium.Marker(Location,color='blue' ,popup=jsonCleaningData3['num'][i], tooltip=jsonCleaningData3['name'][i],icon=folium.Icon(color="green")) 
-          marker.add_to(map_data)
-        i=i+1
-        count += 1
-        print(count)
+    #count = 0
+
+    for i in range(0, len(jsonCleaningData)):
+      Location=[jsonCleaningData['lat'][i],jsonCleaningData['lng'][i]]
+      marker= folium.Marker(Location,color='blue' ,popup=jsonCleaningData['num'][i], tooltip=jsonCleaningData['name'][i],icon=folium.Icon(color="green"))
+      marker.add_to(map_data)
+
+    for i in range(0, len(jsonCleaningData2)):
+      Location=[jsonCleaningData2['lat'][i],jsonCleaningData2['lng'][i]]
+      marker= folium.Marker(Location, popup=jsonCleaningData2['num'][i], tooltip=jsonCleaningData2['name'][i],icon=folium.Icon(color="red"))
+      marker.add_to(map_data)
+      #count += 1
+      #print(count)
+
     map_data.save(r'c:\module1\navermap4.html')
   
     print("[%s] 저장 성공 : " % datetime.datetime.now())
